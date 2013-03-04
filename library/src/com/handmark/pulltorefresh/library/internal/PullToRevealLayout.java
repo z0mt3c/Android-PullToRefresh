62b680b383fe627e95f8843ebad7bda958dac4d8
Nick Burton <nick@foursquare.com>
2013-03-04 12:56:10 -0500

    WIP commit.

---
+++ library/src/com/handmark/pulltorefresh/library/internal/PullToRevealLayout.java
@@ -0,0 +1,48 @@
+package com.handmark.pulltorefresh.library.internal;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Orientation;
+
+/**
+ * @date   Mar 1, 2013
+ * @author Nick Burton (thatsburto.com)
+ */
+public class PullToRevealLayout extends LoadingLayout {
+    
+    public PullToRevealLayout(Context context, final Mode mode, final Orientation scrollDirection, TypedArray attrs) {
+        super(context, mode, scrollDirection, attrs);        
+    }
+
+    @Override
+    protected void refreshingImpl() {
+    }
+    
+    @Override
+    protected void resetImpl() {
+    }
+
+    @Override
+    protected int getDefaultDrawableResId() {
+        return 0;
+    }
+
+    @Override
+    protected void onLoadingDrawableSet(Drawable imageDrawable) {
+    }
+
+    @Override
+    protected void onPullImpl(float scaleOfLayout) {
+    }
+
+    @Override
+    protected void pullToRefreshImpl() {
+    }
+
+    @Override
+    protected void releaseToRefreshImpl() {
+    }
+}
